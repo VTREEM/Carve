81d5b8a5b01127c9e3e54c00d481db5b75b1104d
Tobias Sargeant <tobias.sargeant@gmail.com>
2012-01-09 23:51:10 -0500

    the header stdint.h exists as of vs2010.

--- include/carve/win32.h
+++ include/carve/win32.h
@@ -5,14 +5,6 @@
 #pragma warning (disable : 4996)
 #pragma warning (disable : 4786)
 
-typedef char int8_t;
-typedef short int16_t;
-typedef long int32_t;
-
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned long uint32_t;
-
 #include <string.h>
 #include <stdlib.h>
 
@@ -28,18 +20,29 @@ inline long random() {
   return rand();
 }
 
+#if defined(_MSC_VER)
+#  include <carve/cbrt.h>
+
+#if _MSC_VER < 1300
 // intptr_t is an integer type that is big enough to hold a pointer
 // It is not defined in VC6 so include a definition here for the older compiler
-#if defined(_MSC_VER) && _MSC_VER < 1300
 typedef long intptr_t;
 typedef unsigned long uintptr_t;
 #endif
 
-#if defined(_MSC_VER)
+#  if _MSC_VER < 1600
+// stdint.h is not available before VS2010
+typedef char int8_t;
+typedef short int16_t;
+typedef long int32_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned long uint32_t;
+
 typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
-#endif
-
-#if defined(_MSC_VER)
-#  include <carve/cbrt.h>
+#  else
+#    include <stdint.h>
+#  endif
 #endif
