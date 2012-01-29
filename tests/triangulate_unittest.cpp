8828f611b71ddd61e3fd7af7a1ad5f80f14202de
Tobias Sargeant <tobias.sargeant@gmail.com>
2012-01-29 12:45:18 -0500

    Add triangulator unittest for degenerate quad.

--- tests/triangulate_unittest.cpp
+++ tests/triangulate_unittest.cpp
@@ -24,6 +24,18 @@
 #include <carve/triangulator.hpp>
 #include <carve/geom.hpp>
 
+TEST(Triangulate, Test2) {
+  std::vector<carve::geom::vector<2> > poly;
+  std::vector<carve::triangulate::tri_idx> result;
+
+  poly.push_back(carve::geom::VECTOR(0.0614821249999999985824672,0.0192046249999999994739763));
+  poly.push_back(carve::geom::VECTOR(0.0480376250000000004636291,0.0355867500000000003268497));
+  poly.push_back(carve::geom::VECTOR(0.0316555000000000030802028,0.0490312499999999984456878));
+  poly.push_back(carve::geom::VECTOR(0.0480376250000000004636291,0.0355867500000000003268497));
+
+  carve::triangulate::triangulate(poly, result);
+}
+
 TEST(Triangulate, Test1) {
   std::vector<carve::geom::vector<2> > poly;
   std::vector<carve::triangulate::tri_idx> result;
